9cf8c36d3a75332c68f779e1c1251cd1a61928b3
Venkat-737 <venkatsrisai670@gmail.com>
2023-11-14 21:59:31 +0530

    Created navbar and used react routes for endpoints and text-color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./pages/login.jsx";
+import Navbar from "./components/Navbar.jsx";
+import {Route, Routes} from 'react-router-dom';
+import Developers from "./pages/developers.jsx";
+import Community from "./pages/community.jsx";
+import Roadmap from "./pages/roadmap.jsx";
+import Ecosystem from "./pages/ecosystem.jsx";
+import DApps from "./pages/dapps.jsx";
+import Resources from "./pages/resources.jsx";
+import Getstarted from "./pages/getstarted.jsx";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +20,18 @@ function App() {
   return (
     <>
       <Login />
+      <Navbar/>
+      <div className="container">
+        <Routes>
+        <Route path="/developers" element ={<Developers/>}/>
+        <Route path="/community" element ={<Community/>}/>
+        <Route path="/roadmap" element ={<Roadmap/>}/>
+        <Route path="/ecosystem" element ={<Ecosystem/>}/>
+        <Route path="/dapps" element ={<DApps/>}/>
+        <Route path="/resources" element ={<Resources/>}/>
+        <Route path="/getstarted" element={<Getstarted/>}/>
+        </Routes>
+      </div>
     </>
   );
 }

ed558bb429af12573d7537c658a7e44cdff9beca
harsha6345 <harshab6345@gmail.com>
2023-11-16 14:10:48 +0530

    Added test environment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./pages/login.jsx";
 import Navbar from "./components/Navbar.jsx";
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Developers from "./pages/developers.jsx";
 import Community from "./pages/community.jsx";
 import Roadmap from "./pages/roadmap.jsx";
@@ -12,7 +12,7 @@ import Ecosystem from "./pages/ecosystem.jsx";
 import DApps from "./pages/dapps.jsx";
 import Resources from "./pages/resources.jsx";
 import Getstarted from "./pages/getstarted.jsx";
-
+import TestEnv from "./pages/TestEnv.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,16 +20,17 @@ function App() {
   return (
     <>
       <Login />
-      <Navbar/>
+      <Navbar />
       <div className="container">
         <Routes>
-        <Route path="/developers" element ={<Developers/>}/>
-        <Route path="/community" element ={<Community/>}/>
-        <Route path="/roadmap" element ={<Roadmap/>}/>
-        <Route path="/ecosystem" element ={<Ecosystem/>}/>
-        <Route path="/dapps" element ={<DApps/>}/>
-        <Route path="/resources" element ={<Resources/>}/>
-        <Route path="/getstarted" element={<Getstarted/>}/>
+          <Route path="/developers" element={<Developers />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/roadmap" element={<Roadmap />} />
+          <Route path="/ecosystem" element={<Ecosystem />} />
+          <Route path="/dapps" element={<DApps />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/getstarted" element={<Getstarted />} />
+          <Route path="/test" element={<TestEnv />} />
         </Routes>
       </div>
     </>
--- frontend/src/pages/TestEnv.jsx
+++ frontend/src/pages/TestEnv.jsx
@@ -2,9 +2,11 @@ import Editor from "@monaco-editor/react";
 export default function TestEnv() {
   return (
     <Editor
-      height="90vh"
+      height="400px"
+      width="500px"
       defaultLanguage="javascript"
       defaultValue="// some comment"
+      theme="vs-dark"
     />
   );
 }

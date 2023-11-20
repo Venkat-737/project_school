0a445acdb3df1c4734efd62a572d87428ac9194c
Venkat-737 <venkatsrisai670@gmail.com>
2023-11-20 10:09:57 +0530

    Created Login component with animation  and some part of Footer component using react, tailwindcss

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import "../App.css";
 import { Link, useLocation } from "react-router-dom";
+import skillSyncLogoCropped from '../assets/skillsynclogocropped.jpeg'
 
 export default function Navbar() {
   const loc = useLocation();
@@ -11,37 +12,38 @@ export default function Navbar() {
       <nav className="justify-between bg-background font-poppins flex flex-row items-center  ${isActive('/')} ">
         <div className="logo">
           <img
-            src="https://picsum.photos/120/80?random=1"
-            className="my-3 ml-6 mini:ml-16"
+            src={skillSyncLogoCropped}
+            style={{width: '80px', height:'50px' }}
+            className="my-3 ml-6 mini:ml-8 object-scale-down"
           />
         </div>
         <div className="options gap-6 text-text wb:flex hidden">
-          <Link to="/developers" className={isActive("/developers")}>
+          <Link to="/developers" className={`${isActive("/developers")} hover:text-primary transition`}>
             Developers
           </Link>
-          <Link to="/ecosystem" className={isActive("/ecosystem")}>
+          <Link to="/ecosystem" className={`${isActive("/ecosystem")} hover:text-primary transition`}>
             Ecosystem
           </Link>
-          <Link to="/community" className={isActive("/community")}>
+          <Link to="/community" className={`${isActive("/community")}  hover:text-primary transition`} hover>
             Community
           </Link>
-          <Link to="/dapps" className={isActive("/dapps")}>
+          <Link to="/dapps" className={`${isActive("/dapps")}  hover:text-primary transition`}>
             DApps
           </Link>
-          <Link to="/roadmap" className={isActive("/roadmap")}>
+          <Link to="/roadmap" className={`${isActive("/roadmap")}  hover:text-primary transition`}>
             Roadmap
           </Link>
-          <Link to="/resources" className={isActive("/resources")}>
+          <Link to="/resources" className={`${isActive("/resources")} hover:text-primary transition`}>
             Resource
           </Link>
-          <Link to="/test" className={isActive("/test")}>
+          <Link to="/test" className={`${isActive("/test")}  hover:text-primary transition`}>
             Test
           </Link>
         </div>
         <div className="hamburger wb:hidden flex text-text">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="mr-6 mini:mr-12"
+            className="mr-6 "
             width="32"
             height="32"
             viewBox="0 0 24 24"
@@ -59,7 +61,7 @@ export default function Navbar() {
         <div className="getstarted hidden wb:flex">
           <Link
             to="/pages"
-            className="getstartedbtn cursor-pointer mr-16 items-center justify-center  bg-primary text-text w-32 h-10 wb:flex hidden"
+            className="getstartedbtn  cursor-pointer mr-8 items-center justify-center  bg-primary text-text w-32 h-10 wb:flex hidden"
           >
             Get Started
           </Link>
